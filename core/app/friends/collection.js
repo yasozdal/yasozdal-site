e80dba3a3d9ecdcf3b58917701f236544c2277af
Denis Chusovitin <Chusovitin.Den@gmail.com>
2014-12-09 00:32:22 +0300

    Added friends collection

---
+++ core/app/friends/collection.js
@@ -0,0 +1,39 @@
+define(['backbone', 'app/config'], function (Backbone, config) {
+
+    'use strict';
+
+    return Backbone.Collection.extend({
+
+        //url: config.API + 'friends/my/m',
+
+        url: 'http://nbixman-001-site1.myasp.net/api/' + 'friends/my/', //new server URL
+
+        //more: function() {
+        //this.fetch({ remove: false, data: { count: 1, offset: this.length }, processData: true });
+        //   this.fetch({ data: { count: 1 + this.length }, processData: true });
+        //},
+        //},
+
+        initialize: function() {
+            this.fetch({url : this.url + 'm'});
+        },
+
+        following: function() {
+            this.fetch({url : this.url + 's'});
+        },
+
+        followers: function() {
+            this.fetch({url : this.url + 'f'});
+        },
+
+        mf: function() {
+            this.fetch({url : this.url + 'mf'});
+        },
+
+        ms: function() {
+            this.fetch({url : this.url + 'ms'});
+        }
+
+    });
+
+});
