be89d52dd32688f0dd58774781328f6f3ec78f4f
Denis Chusovitin <Chusovitin.Den@gmail.com>
2014-12-25 10:14:06 +0300

    Add follow/unfollow

--- core/app/friends/model.js
+++ core/app/friends/model.js
@@ -1,9 +1,17 @@
-define(['backbone'], function (Backbone) {
+define(['backbone', 'app/confiq'], function (Backbone, confiq) {
 
     'use strict';
 
     return Backbone.Model.extend({
+        url: conqiq.API + 'friends/',
 
+        follow: function(id) {
+            this.fetch({url: this.url + 'follow/' + id});
+        },
+
+        unfollow: function(id) {
+            this.fetch({url: this.url + 'unfollow/' + id});
+        }
     });
 
 });
